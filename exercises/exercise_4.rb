a2b5e29dff643129dc031c40bcde551592f15aa6
Cheryet <C.heryet@outlook.com>
2022-12-12 16:50:20 -0700

    refactor variables to local variables

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -8,27 +8,27 @@ puts "----------"
 
 # Your code goes here ...
 
-store1 = Store.new
-store1.name = "Surrey"
-store1.annual_revenue = 224000
-store1.mens_apparel = false
-store1.womens_apparel = true
+@store1 = Store.new
+@store1.name = "Surrey"
+@store1.annual_revenue = 224000
+@store1.mens_apparel = false
+@store1.womens_apparel = true
 
-store2 = Store.new
-store2.name = "Whistler"
-store2.annual_revenue = 1900000
-store2.mens_apparel = true
-store2.womens_apparel = false
+@store2 = Store.new
+@store2.name = "Whistler"
+@store2.annual_revenue = 1900000
+@store2.mens_apparel = true
+@store2.womens_apparel = false
 
-store3 = Store.new
-store3.name = "Yaletown"
-store3.annual_revenue = 430000
-store3.mens_apparel = true
-store3.womens_apparel = true
+@store3 = Store.new
+@store3.name = "Yaletown"
+@store3.annual_revenue = 430000
+@store3.mens_apparel = true
+@store3.womens_apparel = true
 
-store1.save
-store2.save
-store3.save
+@store1.save
+@store2.save
+@store3.save
 
 mens_stores = Store.where(mens_apparel: true)
 
